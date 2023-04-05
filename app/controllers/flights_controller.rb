ca5b005b1b6d8f6213bad2b5b52002cf77b62aa9
amrxt1 <amrxt11@outlook.com>
2023-04-05 14:51:06 +0530

    Fix flash[:error] position

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,7 +6,7 @@ class FlightsController < ApplicationController
     if !params.has_key?(:arrival_airport)
       @results = nil
     elsif [params[:departing_airport].blank?, params[:arrival_airport].blank?, params[:date].blank?].all?
-      flash.now[:error] = "Select at least one condition"
+      flash.now[:error] = "Please select at least one of the three options"
     else
       @results = Flight.where({departing_airport: params[:departing_airport], arrival_airport: params[:arrival_airport]}.reject{ |_, v| v.blank? }) 
     end
