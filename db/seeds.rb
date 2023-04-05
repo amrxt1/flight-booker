ce4fcf3b926789ca42043674230a1d70833e4d7a
amrxt1 <amrxt11@outlook.com>
2023-04-05 11:10:27 +0530

    Create Airport model and generate data using seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+airport_codes = [
+    "JFK",
+    "LHR",
+    "CDG",
+    "HND",
+    "SYD",
+    "MUC",
+    "ORD",
+    "SFO",
+    "DXB",
+    "BKK",
+    "AMS",
+    "FRA",
+    "PEK",
+    "ICN",
+    "MEL",
+    "BOM",
+    "GRU",
+    "YYZ",
+    "MAD",
+    "BCN"
+  ]
+
+#Airport.delete_all
+
+#airport_codes.each do |code|
+#    Airport.create( code: code )
+#end
