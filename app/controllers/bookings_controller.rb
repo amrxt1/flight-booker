7829fabe925ca36283c150f84a7f394b53c260d2
amrxt1 <amrxt11@outlook.com>
2023-05-28 17:22:06 +0530

    Make the ticket flow work smoothly.
    Create forms for creating bookings with fields for the given number of passengers,

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    params[:passengers].to_i.times{ @booking.passengers.build }
+    @flight = Flight.find( params[:flight_id] )
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+        redirect_to @booking, notice: "You have sucessfully booked the flight!"
+    else
+        render :new, status: :unprocessable_entity  
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "flights#index"
   resources :flights , only: :index
+  resources :bookings
 end
